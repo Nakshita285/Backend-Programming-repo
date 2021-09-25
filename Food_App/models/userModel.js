8dfbaba79665b45e5a6063b180d6c96a104e7a2c
Nakshita <nakshita.malhotra.61@gmail.com>
2021-09-26 00:22:50 +0530

    model and schema basics

--- Food_App/models/userModel.js
+++ Food_App/models/userModel.js
@@ -3,11 +3,13 @@
 const mongoose = require('mongoose');
 let { DB_LINK } = require('../secrets');
 
+const validator = require('email-validator');
+
 // connect server to the Mongoose       
                                         // now we dont need this
 mongoose.connect(DB_LINK, {})           // previous parameters -> useNewUrlParser : true, useCreateIndex : true, useUnifiedTopology: true;
 .then(function(database){
-    console.log(database);
+    console.log("Database is Created with user details!");
 })
 .catch(function(error){
     console.log("Error: ", error);
@@ -15,7 +17,51 @@ mongoose.connect(DB_LINK, {})           // previous parameters -> useNewUrlParse
 
 // Moogoose is an async function which provides you a promise on which we use then and catch function. 
 
+// Now we are going to Connect the mongoDB with the application
 
+// UserSchema && userModal
 
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique:true,
+        validator: function(){
+            validator.validate(this.email);
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 5,
 
+    },
+    confirmPassword:{
+        type: String,
+        requried: true,
+        validator: function(){
+            return this.password == this.confirmPassword;
+        }
+    }   
+})
 
+const userModel = new mongoose.model('userModel', userSchema);
+
+(async function createUserDB(){
+    let user = await userModel.create({
+        name : "Raghav",
+        age: 27,
+        email: "raghavJuneja@gmail.com",
+        password: "abcdef",
+        confirmPassword: "abcdef",
+    });
+    console.log("User created: ", user);
+})();
